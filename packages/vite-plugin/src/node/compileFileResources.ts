89677ab39d10315cbfa4945483b4dd59f35535a2
Kyohei Fukuda <kyouhei.fukuda0729@gmail.com>
2023-05-04 16:18:53 +0900

    FIx RangeError: Maximum call stack size exceeded in compileFileResources

--- packages/vite-plugin/src/node/compileFileResources.ts
+++ packages/vite-plugin/src/node/compileFileResources.ts
@@ -27,35 +27,52 @@ export function compileFileResources(
     imports: new Set(),
   },
 ): FileResources {
-  const chunk = chunks.get(fileName)
-  if (chunk) {
-    const { modules, facadeModuleId, imports, dynamicImports } = chunk
-    for (const x of imports) resources.imports.add(x)
-    for (const x of dynamicImports) resources.imports.add(x)
-    for (const x of [...imports, ...dynamicImports])
-      compileFileResources(x, { chunks, files, config }, resources)
-    for (const m of Object.keys(modules))
-      if (m !== facadeModuleId) {
-        const key = prefix('/', relative(config.root, m.split('?')[0]))
-        const script = contentScripts.get(key)
-        if (script)
-          if (typeof script.fileName === 'undefined') {
-            throw new Error(`Content script fileName for ${m} is undefined`)
-          } else {
-            resources.imports.add(script.fileName)
-            compileFileResources(
-              script.fileName,
-              { chunks, files, config },
-              resources,
-            )
-          }
+  const stack = [fileName];
+
+  while (stack.length) {
+    const currentFileName = stack.pop() || "";
+    const chunk = chunks.get(currentFileName);
+
+    if (chunk) {
+      const { modules, facadeModuleId, imports, dynamicImports } = chunk;
+      for (const x of imports) resources.imports.add(x);
+      for (const x of dynamicImports) resources.imports.add(x);
+
+      const allImports = [...imports, ...dynamicImports];
+      for (const x of allImports) {
+        if (!resources.imports.has(x)) {
+          resources.imports.add(x);
+          stack.push(x);
+        }
       }
+
+      for (const m of Object.keys(modules)) {
+        if (m !== facadeModuleId) {
+          const key = prefix('/', relative(config.root, m.split('?')[0]));
+          const script = contentScripts.get(key);
+          if (script) {
+            if (typeof script.fileName === 'undefined') {
+              throw new Error(`Content script fileName for ${m} is undefined`);
+            } else {
+              resources.imports.add(script.fileName);
+
+              if (!resources.imports.has(script.fileName)) {
+                resources.imports.add(script.fileName);
+                stack.push(script.fileName);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    const file = files.get(currentFileName);
+    if (file) {
+      const { assets = [], css = [] } = file;
+      for (const x of assets) resources.assets.add(x);
+      for (const x of css) resources.css.add(x);
+    }
   }
-  const file = files.get(fileName)
-  if (file) {
-    const { assets = [], css = [] } = file
-    for (const x of assets) resources.assets.add(x)
-    for (const x of css) resources.css.add(x)
-  }
-  return resources
+
+  return resources;
 }
